a09637864b198bdfe54055db36cc83e5754d4739
luoye10 <511645424@qq.com>
2023-03-23 22:22:21 +0800

    collapse

---
+++ src/components/CollapsePage.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <div class="title">基础</div>
+    <div class="content">
+      <ul class="itemList">
+        <li class="item" v-for="item in items" :key="item.massage">
+          <div class="main" @click="show(item)">
+            <i class="el-icon-arrow-right" v-if="!item.isShow"></i>
+            <i class="el-icon-arrow-down" v-else></i>
+            <div class="rank">{{ item.rank }}</div>
+          </div>
+          <div class="remark" v-show="item.isShow">{{ item.remark }}</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          rank: '青铜',
+          remark: '可以',
+          isShow: false,
+        },
+        {
+          rank: '白银',
+          remark: '很好',
+          isShow: false,
+        },
+        {
+          rank: '黄金',
+          remark: '真棒',
+          isShow: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    show(item) {
+      setTimeout(() => {
+        item.isShow = !item.isShow;
+      }, 100);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.container {
+  width: 300px;
+  border: 1px solid #dadada;
+  margin: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  .title {
+    height: 50px;
+  }
+  .content {
+    flex: 1;
+  }
+  .itemList {
+    display: flex;
+    flex-direction: column;
+    .item {
+      flex: 1;
+      padding: 20px 0;
+      display: flex;
+      flex-direction: column;
+      box-sizing: border-box;
+      .main {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        .el-icon-arrow-right,
+        .el-icon-arrow-down {
+          width: 30px;
+        }
+        .rank {
+          flex: 1;
+        }
+      }
+      .remark {
+        padding-top: 20px;
+      }
+    }
+  }
+  .item:nth-child(2) {
+    border-top: 1px solid #dadada;
+    border-bottom: 1px solid #dadada;
+  }
+}
+</style>
