95401ea801bd48775e79a708680d028bc11aecd0
luoye10 <511645424@qq.com>
2023-03-29 20:07:22 +0800

    click show pop

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <layer-page></layer-page>
     <floor-page></floor-page>
     <drop-down></drop-down>
+    <text-ellipsis></text-ellipsis>
   </div>
 </template>
 <script>
@@ -11,12 +12,14 @@ import CollapsePage from './components/CollapsePage.vue';
 import LayerPage from './components/LayerPage.vue';
 import FloorPage from './components/FloorPage.vue';
 import DropDown from './components/DropDown.vue';
+import TextEllipsis from './components/TextEllipsis.vue';
 export default {
   components: {
     CollapsePage,
     LayerPage,
     FloorPage,
     DropDown,
+    TextEllipsis,
   },
   mounted() {
     console.log(this.BigNumber.plus(0.1).plus(0.2).toNumber());
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -3,19 +3,35 @@
     <div class="text">基础用法</div>
     <div
       :class="['test', { active: isHover }, { add: isAdd }]"
-      @mouseenter="open"
+      @mouseover="open"
       @mouseout="recover"
       @click="add"
     >
       找个地方休息
     </div>
     <transition name="zyh">
-      <ul class="itemList" v-show="isShow">
-        <li class="item" v-for="item in items" :key="item.massage">
-          {{ item }}
+      <ul
+        class="itemList"
+        v-show="isShow"
+        @mouseover="open"
+        @mouseout="recover"
+      >
+        <li
+          :class="['item', { background: item.isAppend }]"
+          v-for="(item, index) in items"
+          :key="item.massage"
+          @mouseover="append(item)"
+          @mouseout="remove(item)"
+          @click="show(index)"
+        >
+          {{ item.text }}
         </li>
       </ul>
     </transition>
+    <div class="pop" v-show="isOpen">
+      <div class="icon">!</div>
+      <div class="word"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,14 +39,33 @@ export default {
   data() {
     return {
       items: [
-        '滨海湾金沙，新加坡',
-        '布朗酒店，伦敦',
-        '亚特兰蒂斯巴哈马，拿骚',
-        '比利福山庄酒店，洛杉矶',
+        {
+          text: '滨海湾金沙，新加坡',
+          isAppend: false,
+        },
+        {
+          text: '布朗酒店，伦敦',
+          isAppend: false,
+        },
+        {
+          text: '亚特兰蒂斯巴哈马，拿骚',
+          isAppend: false,
+        },
+        {
+          text: '比利福山庄酒店，洛杉矶',
+          isAppend: false,
+        },
+      ],
+      lists: [
+        'marina bay sands',
+        "brown's hotel, london",
+        'atlantis nahamas, nassau',
+        'the beverly hills hotel, los angeles',
       ],
       isShow: false,
       isHover: false,
       isAdd: false,
+      isOpen: false,
     };
   },
   methods: {
@@ -48,11 +83,59 @@ export default {
         this.isAdd = false;
       }, 100);
     },
-    show() {},
+    append(item) {
+      this.items.forEach((list) => {
+        if (item === list) {
+          item.isAppend = true;
+        } else {
+          list.isAppend = false;
+        }
+      });
+    },
+    remove(item) {
+      item.isAppend = false;
+    },
+    show(index) {
+      let word = document.getElementsByClassName('word')[0];
+      this.lists.forEach((list, num) => {
+        if (index === num) {
+          word.innerHTML = list;
+        }
+      });
+      this.isOpen = true;
+      setTimeout(() => {
+        this.isOpen = false;
+      }, 2000);
+    },
   },
 };
 </script>
 <style lang="less" scoped>
+.pop {
+  height: 50px;
+  border-radius: 5px;
+  background: #dadada;
+  position: fixed;
+  left: 50%;
+  top: 10px;
+  transform: translate(-50%, 0);
+  padding: 15px 30px;
+  box-sizing: border-box;
+  display: flex;
+  .icon {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: blue;
+    color: white;
+    text-align: center;
+    line-height: 20px;
+    margin-right: 20px;
+  }
+  .text {
+    flex: 1;
+  }
+}
 .box {
   width: 300px;
   height: 100px;
@@ -93,6 +176,9 @@ export default {
   }
   .add {
     box-shadow: 0 0 2px 2px greenyellow;
+  }
+  .background {
+    background: #dadada;
   }
   .zyh-enter-active,
   .zyh-leave-active {
